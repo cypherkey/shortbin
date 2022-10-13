028bbb7e25f46199f1608e8d0f5d71f551a1bc87
Aaron Anderson <aaron@netopia.ca>
2022-10-13 05:08:05 -0600

    Update version to 5

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -it --rm -u 1000:1000 -v ${PWD}:/mnt maven:3-jdk-8 /bin/bash /mnt/build.sh
-docker build -t registry.netopia.ca/shortbin:1.2 .
+docker run -it --rm -u 1000:1000 -v ${PWD}:/mnt maven:3-jdk-11 /bin/bash /mnt/build.sh
+docker build -t registry.netopia.ca/shortbin:5 .
