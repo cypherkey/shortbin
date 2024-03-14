51df408ff48c1cf41e33725af437db24c80d1fd5
Aaron Anderson <aaron@netopia.ca>
2024-03-14 17:56:13 -0600

    Fix Docker build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -it --rm -u 1000:1000 -v ${PWD}:/mnt maven:3-eclipse-temurin-21 /bin/bash /mnt/build.sh
+docker run -it --rm -v ${PWD}:/mnt maven:3-eclipse-temurin-21 /bin/bash /mnt/build.sh
 docker build -t registry.netopia.ca/shortbin:7 .
